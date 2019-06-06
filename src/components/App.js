bca5e35b3651e7ccab88af8538e718b630446364
anthonygharvey <anthony@anthonygharvey.com>
2019-06-05 23:23:52 -0400

    Added api call to backend
    Converted App to a class component and added state
    Added fetch call to api for random stock

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import Navbar from "./Navigation";
-import LogoDisclaimer from "./LogoDisclaimer";
 
-const App = () => {
-  return (
-    <>
-      <Navbar />
-    </>
-  );
-};
+class App extends Component {
+  state = {
+    user_name: "",
+    stock: [],
+    portfolio: {
+      cash: 10000,
+      shares: 0,
+      stock: ""
+    }
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:4000/api/random_stock")
+      .then(response => response.json())
+      .then(stock =>
+        this.setState(prevState => ({
+          ...prevState,
+          stock: stock,
+          portfolio: {
+            ...prevState.portfolio,
+            stock: stock.symbol
+          }
+        }))
+      );
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar />
+      </>
+    );
+  }
+}
 
 export default App;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,11 +5,11 @@ import { ReactComponent as Logo } from "./logo.svg";
 const Navbar = () => {
   return (
     // <Logo />
-    <div class="ui menu">
+    <div className="ui menu">
       <div style={{ height: "40px", width: "40px" }}>
         <Logo />
       </div>
-      <div class="header item">Anoymous Stock Trading Game</div>
+      <div className="header item">Anoymous Stock Trading Game</div>
     </div>
   );
 };
