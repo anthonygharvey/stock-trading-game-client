e032a2b13bc8ff27c8a0d6bfde34eea8c0b7746e
anthonygharvey <anthony@anthonygharvey.com>
2019-06-30 16:16:48 -0400

    Added About and Leaderboard components and routes

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Link } from "react-router-dom";
 import Navbar from "./Navigation";
 import Portfolio from "./Portfolio";
 import Chart from "./Chart";
+import About from "./About";
+import Leaderboard from "./Leaderboard";
 
 class App extends Component {
   render() {
@@ -12,6 +14,8 @@ class App extends Component {
           <Route path="/" exact component={Navbar} />
           <Route path="/" exact component={Portfolio} />
           <Route path="/" exact component={Chart} />
+          <Route path="/about" exact component={About} />
+          <Route path="/leaderboard" exact component={Leaderboard} />
         </BrowserRouter>
       </>
     );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,25 @@
 import React from "react";
-// import Logo from './'
+import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "./logo.svg";
 
 const Navbar = () => {
   return (
-    // <Logo />
     <div className="ui menu">
       <div style={{ height: "40px", width: "40px" }}>
-        <Logo />
+        <Link to="/">
+          <Logo />
+        </Link>
       </div>
       <div className="header item">Anoymous Stock Trading Game</div>
+
+      <div className="right menu">
+        <Link to="/leaderboard">
+          <div1 className="item">Leaderboard</div1>
+        </Link>
+        <Link to="/about">
+          <div1 className="item">About</div1>
+        </Link>
+      </div>
     </div>
   );
 };
